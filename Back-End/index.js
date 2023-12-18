947165674ce9a765b8a335d4e31c64baf96eef70
liam-powers <95184359+liam-powers@users.noreply.github.com>
2023-12-17 22:54:49 -0500

    12/17 Initial Setup

---
+++ Back-End/index.js
@@ -0,0 +1,9 @@
+//Node.js home base
+
+//TODO: Sync with MongoDB (mongosync.js)
+
+//TODO: Webscrape with Puppeteer (webscrape.js)
+
+//TODO: Filter with own logic/sorting algorithms/nearest path (Dijkstra's? Maps?) (filter.js)
+
+console.log("Hey, index.js is working!");
